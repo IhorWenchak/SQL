94784578427ecd89f16b03f6914230f25c397c99
Ihor Venchak <iwenchak@gmail.com>
2020-09-28 14:36:59 +0300

    ...

--- SQLQuery11.sql
+++ SQLQuery11.sql
@@ -2,4 +2,4 @@
 SELECT * FROM FacultyMathematicDB.dbo.STUDENTS st
 INNER JOIN GROUPS gr ON gr.GROUP_ID = st.GROUP_ID
 INNER JOIN COURSES cours ON cours.COURSE_ID = gr.COURSE_ID
-WHERE cours.NAME = 'HTML5 è CSS3 Starter'
+WHERE cours.NAME = 'Java Professional Renewed'
